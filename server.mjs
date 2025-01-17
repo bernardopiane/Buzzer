5121f49097c5f1452228d07197b03ef64aedc0aa
Bernardo Piane <bernardopiane@users.noreply.github.com>
2025-01-17 12:35:48 -0300

    Admin authentication

--- server.mjs
+++ server.mjs
@@ -29,7 +29,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/admin', (req, res) => {
-    res.sendFile(join(__dirname, 'admin.html'));
+    const auth = { login: 'admin', password: 'password' }; // Default credentials
+
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+    if (login && password && login === auth.login && password === auth.password) {
+        res.sendFile(join(__dirname, 'admin.html'));
+    } else {
+        res.set('WWW-Authenticate', 'Basic realm="401"');
+        res.status(401).send('Authentication required.');
+    }
 });
 
 // Socket.io event handlers
